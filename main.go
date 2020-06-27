6874318db69770d188ae535de2df58adc501ea44
iteny <iteny@msn.com>
2020-06-27 21:27:46 +0800

    kkk

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pongjhfjhgfghdfgh",
+			"message": "pongjhfj00099999",
 		})
 	})
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
