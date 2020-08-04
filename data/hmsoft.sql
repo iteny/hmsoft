86033332fab8327e0ab69f928e30226ac44370fa
iteny <iteny@msn.com>
2020-08-04 23:01:18 +0800

    fddsfa

--- data/hmsoft.sql
+++ data/hmsoft.sql
@@ -11,11 +11,33 @@
  Target Server Version : 80021
  File Encoding         : 65001
 
- Date: 02/08/2020 22:23:38
+ Date: 04/08/2020 23:00:58
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for hm_login_logs
+-- ----------------------------
+DROP TABLE IF EXISTS `hm_login_logs`;
+CREATE TABLE `hm_login_logs`  (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '用户名',
+  `time` int NOT NULL DEFAULT 0 COMMENT '登录时间',
+  `ip` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '登录ip',
+  `useragent` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '浏览器信息',
+  `uid` bigint NOT NULL COMMENT '登录用户ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of hm_login_logs
+-- ----------------------------
+INSERT INTO `hm_login_logs` VALUES (1, 'admin', 1596548392, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36', 1);
+INSERT INTO `hm_login_logs` VALUES (2, 'admin', 1596552286, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36', 1);
+INSERT INTO `hm_login_logs` VALUES (3, 'admin', 1596552313, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36', 1);
+INSERT INTO `hm_login_logs` VALUES (4, 'admin', 1596552651, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36', 1);
 
 -- ----------------------------
 -- Table structure for hm_users
