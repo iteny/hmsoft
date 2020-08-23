b74e33c6c2764e9a66f7cffc093d9233851b62ac
iteny <iteny@msn.com>
2020-08-23 22:39:02 +0800

    的反对改革

--- view/admin/src/plugins/element.js
+++ view/admin/src/plugins/element.js
@@ -1,10 +1,30 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import { Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Form,
+  FormItem,
+  Input,
+  Button,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  MenuItem,
+  Submenu,
+  MenuItemGroup
+} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.prototype.$message = Message
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
-
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
--- view/admin/src/views/Home.vue
+++ view/admin/src/views/Home.vue
@@ -1,8 +1,50 @@
 <template>
   <div class="home">
-    <el-button type="success" plain>成功按钮</el-button>
-    <el-button type="success" plain>成功按钮</el-button>
-    <el-button type="success" plain>成功按钮</el-button>
+    <el-container>
+      <el-header>
+        <div>
+          <img src="../assets/img/avatar.jpg" />
+          <span>系统管理后台</span>
+        </div>
+        <el-button type="success" @click="logout()" size="mini">退出按钮</el-button>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <!-- 一级菜单 -->
+            <el-submenu index="1">
+              <!-- 一级菜单模板 -->
+              <template slot="title">
+                <!-- 菜单图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>导航一</span>
+              </template>
+              <!-- 一级菜单下二级菜单 -->
+              <el-submenu index="1-4">
+                <template slot="title">
+                  <!-- 菜单图标 -->
+                  <i class="el-icon-location"></i>
+                  <!-- 文本 -->
+                  <span>二级菜单</span>
+                </template>
+
+                <!-- <el-menu-item index="1-4-1">选项1</el-menu-item> -->
+              </el-submenu>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -47,3 +89,36 @@ export default {
   },
 };
 </script>
+<style scoped>
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background: chocolate;
+  line-height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.el-header img {
+  widows: 40px;
+  height: 40px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 1px;
+  box-shadow: 0 0 10px #ddd;
+  margin-right: 25px;
+}
+.el-header div {
+  display: flex;
+  align-items: center;
+}
+.el-aside {
+  background: cornflowerblue;
+  line-height: 200px;
+}
+.el-main {
+  background: darkgreen;
+  line-height: 200px;
+}
+</style>
